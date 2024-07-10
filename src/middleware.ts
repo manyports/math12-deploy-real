8f11e37f3a9fef422d13e09b6a910c9fa453e01b
ера <157960666+manyports@users.noreply.github.com>
2024-07-10 20:48:10 +0500

    fix: relocate middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+import cookie from 'cookie';
+
+export function middleware(request: NextRequest) {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get("token");
+
+    const isAuth = !!token;
+    const pathname = request.nextUrl.pathname;
+
+    const isAuthPage = pathname === '/register' || pathname === '/login';
+
+    if (isAuthPage && isAuth) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+
+    if (request.nextUrl.pathname === '/dashboard' && !isAuth) {
+        return NextResponse.redirect(new URL('/register', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/dashboard', '/register', '/login'],
+};
