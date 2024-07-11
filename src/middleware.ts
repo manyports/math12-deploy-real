30ed20428105879a5ab8a34ce58ad89502c7396b
ера <157960666+manyports@users.noreply.github.com>
2024-07-11 21:23:59 +0500

    chore: Update middleware to redirect to register page for additional paths
    This commit updates the middleware to redirect unauthorized users to the register page for additional paths such as '/test', '/imagesolver', and '/chats'. This improves the application's security and user experience.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,32 +4,24 @@ import { NextRequest } from 'next/server';
 import cookie from 'cookie';
 
 export function middleware(request: NextRequest) {
-    const cookiesStore = cookies()
+    const cookiesStore = cookies();
     const token = cookiesStore.get("token");
 
     const isAuth = !!token;
     const pathname = request.nextUrl.pathname;
 
     const isAuthPage = pathname === '/register' || pathname === '/login';
+    const protectedRoutes = ['/dashboard', '/imagesolver', '/chats', '/test'];
 
     if (isAuthPage && isAuth) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
-    if (request.nextUrl.pathname === '/dashboard'  && !isAuth) {
+    if (protectedRoutes.includes(pathname) && !isAuth) {
         return NextResponse.redirect(new URL('/register', request.url));
     }
-        else if(request.nextUrl.pathname === '/chats' && !isAuth){
-            return NextResponse.redirect(new URL('/register', request.url));
-        }
-            else if(request.nextUrl.pathname === '/imagesolver' && !isAuth){
-                return NextResponse.redirect(new URL('/register', request.url));
-            }
-                else if(request.nextUrl.pathname === '/test' && !isAuth){
-                    return NextResponse.redirect(new URL('/register', request.url));
-                }
 }
 
 export const config = {
-    matcher: ['/dashboard', '/register', '/login'],
+    matcher: ['/dashboard', '/register', '/login', '/imagesolver', '/chats', '/test'],
 };
